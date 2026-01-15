83feebc61a5672de01c24f5c6afe089cf19f9817
Mesut Ayhan <mesutayhan1907@gmail.com>
2026-01-15 03:47:05 +0300

    soft delete için global query filter eklendi

--- RealEstateAPI.Data/Context/AppDbContext.cs
+++ RealEstateAPI.Data/Context/AppDbContext.cs
@@ -15,4 +15,16 @@ public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
     public DbSet<PropertyType> PropertyTypes { get; set; }
     public DbSet<PropertyImage> PropertyImages { get; set; }
     public DbSet<Inquiry> Inquiries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Soft Delete için Global Query Filter
+        modelBuilder.Entity<Property>().HasQueryFilter(p => !p.IsDeleted);
+        modelBuilder.Entity<PropertyType>().HasQueryFilter(pt => !pt.IsDeleted);
+        modelBuilder.Entity<PropertyImage>().HasQueryFilter(pi => !pi.IsDeleted);
+        modelBuilder.Entity<Inquiry>().HasQueryFilter(i => !i.IsDeleted);
+        modelBuilder.Entity<AppUser>().HasQueryFilter(u => !u.IsDeleted);
+    }
 }
