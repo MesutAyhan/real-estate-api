6f1b6d513481cd717f08f036199d8525c08f95ff
Mesut Ayhan <mesutayhan1907@gmail.com>
2026-01-15 06:08:19 +0300

    Inquiry ilişkileri ve indexleri eklendi

--- RealEstateAPI.Data/Configurations/InquiryConfiguration.cs
+++ RealEstateAPI.Data/Configurations/InquiryConfiguration.cs
@@ -12,9 +12,19 @@ public class InquiryConfiguration : IEntityTypeConfiguration<Inquiry>
         builder.HasKey(i => i.Id);
 
         builder.Property(i => i.Name).IsRequired().HasMaxLength(100);
-        builder.Property(i => i.Email).IsRequired().HasMaxLength(100);
+        builder.Property(i => i.Email).IsRequired().HasMaxLength(255);
         builder.Property(i => i.Phone).HasMaxLength(20);
         builder.Property(i => i.Message).IsRequired().HasMaxLength(1000);
         builder.Property(i => i.Status).IsRequired();
+
+        // User silindiğinde UserId null olsun (kayıtsız kullanıcı gibi)
+        builder.HasOne(i => i.User)
+            .WithMany(u => u.Inquiries)
+            .HasForeignKey(i => i.UserId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        // Indexler
+        builder.HasIndex(i => i.PropertyId);
+        builder.HasIndex(i => i.Status);
     }
 }
