b2e38ce01c214cc5cd41db9bcc418cbed8f215c3
Mesut Ayhan <mesutayhan1907@gmail.com>
2026-01-15 06:17:56 +0300

    AppUser configuration eklendi

---
+++ RealEstateAPI.Data/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstateAPI.Core.Entities;
+
+namespace RealEstateAPI.Data.Configurations;
+
+public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.LastName).IsRequired().HasMaxLength(50);
+        builder.Property(u => u.ProfilePicture).HasMaxLength(1000);
+        builder.Property(u => u.AgencyName).HasMaxLength(200);
+        builder.Property(u => u.LicenseNumber).HasMaxLength(50);
+    }
+}
